049f7271089034f20532e368917398af538f1aa5
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2025-04-25 20:09:47 +0530

    Version 1.0

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const ejs = require('ejs');
+const jsonfile = require('jsonfile');
+require('./public/App.test.js');
+
+class WEB{
+    constructor(port){
+        this.active = true;
+        this.port = port;
+        this.filename = path.basename(__filename);
+        this.appInfo = jsonfile.readFileSync('./public/manifest.json');
+    }
+}
+
+const app = express();
+let server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+const AppName = "CBCONCORD 25";
+let web = new WEB(PORT);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/images',express.static(path.join(__dirname,'images')));
+app.use('/public',express.static(path.join(__dirname,'public')));
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello");
+});
+
+
+app.get('*', (req, res) => {
+    res.status(404).render('notfound',{error: 404, message: "Page not found on this url, check the source or report it"});
+});
+
+server.listen(PORT, (err) => {
+    if(err) console.log("Oops an error occure:  "+err);
+    console.log(`Compiled successfully!\n\nYou can now view \x1b[33m./${path.basename(__filename)}\x1b[0m in the browser.`);
+    console.info(`\thttp://localhost:${PORT}`);
+    console.log("\n\x1b[32mNode web compiled!\x1b[0m \n");
+});
+
+
+
